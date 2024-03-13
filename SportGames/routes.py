886e2ccaa4d5b692ad7e6ac07a999f367ccbb1f9
Oscar <oscarok2005@gmail.com>
2024-03-13 12:29:23 +0300

    Исправлена ошибка, допущенная в предыдущем коммите.

--- SportGames/routes.py
+++ SportGames/routes.py
@@ -5,6 +5,14 @@ Routes and views for the bottle application.
 from bottle import route, view
 from datetime import datetime
 
+@route('/')
+@view('index')
+def home():
+    """Renders the home page."""
+    return dict(
+        year=datetime.now().year
+    )
+
 @route('/football')
 @view('football')
 def football():
