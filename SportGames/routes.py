cbf528d5d93324f49593316ac1f0552d0d529b0a
Oscar <oscarok2005@gmail.com>
2024-06-04 14:51:12 +0300

    Изменены стили.

--- SportGames/routes.py
+++ SportGames/routes.py
@@ -43,12 +43,12 @@ def chess():
 @view('articles')
 def articles():
     try:
-        # Пытаемся открыть файл "calchistory.json" для чтения
+        # РџС‹С‚Р°РµРјСЃСЏ РѕС‚РєСЂС‹С‚СЊ С„Р°Р№Р» "calchistory.json" РґР»СЏ С‡С‚РµРЅРёСЏ
         with open('static\\articles.json', 'r', encoding='utf-8') as read_json:
-            # Загружаем данные из файла в список articles
+            # Р—Р°РіСЂСѓР¶Р°РµРј РґР°РЅРЅС‹Рµ РёР· С„Р°Р№Р»Р° РІ СЃРїРёСЃРѕРє articles
             articles = json.load(read_json)
     except FileNotFoundError:
-            # Если файл не найден, создаем новый словарь history с пустыми списками для каждого алгоритма    
+            # Р•СЃР»Рё С„Р°Р№Р» РЅРµ РЅР°Р№РґРµРЅ, СЃРѕР·РґР°РµРј РЅРѕРІС‹Р№ СЃР»РѕРІР°СЂСЊ history СЃ РїСѓСЃС‚С‹РјРё СЃРїРёСЃРєР°РјРё РґР»СЏ РєР°Р¶РґРѕРіРѕ Р°Р»РіРѕСЂРёС‚РјР°    
             articles = []
     """Renders the about page."""
     print("NOR")
