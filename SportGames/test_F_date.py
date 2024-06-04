79b4e396042096361002c4d2d3623b55556dfaf7
Oleg Afanasev <afol7935@gmail.com>
2024-06-04 15:32:17 +0300

    Корректировка разметки страницы articles, корректировка стилей, добавление unittest

--- SportGames/test_F_date.py
+++ SportGames/test_F_date.py
@@ -1,8 +1,12 @@
 import unittest
+import addArticle
 
-class Test_test_F_date(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+list_date_incor = ["", "asasdasdsad", 1231, "20/02/2002" ]
+
+class Test_text_F_date(unittest.TestCase):
+    def test_incorrect_date(self):
+        for i in list_date_incor:
+            self.assertFalse(addArticle.isValidDate(i))
 
 if __name__ == '__main__':
     unittest.main()
